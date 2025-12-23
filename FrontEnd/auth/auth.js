3d132d5270e3cb59b228d2aa3df184f5b4511d15
Magdau5 <magdau5nelysis@gmail.com>
2025-12-23 21:28:16 +0100

    Add : JS edition, filters Modif : login => logout JS

--- FrontEnd/auth/auth.js
+++ FrontEnd/auth/auth.js
@@ -1,12 +1,35 @@
-const authLink = document.getElementById("auth-link");
-const token = localStorage.getItem("token");
+const authLink = document.getElementById('auth-link');
+const token = localStorage.getItem('token');
 
-if (authLink && token) {
-	authLink.textContent = "logout";
-	authLink.href = "#";
+const editionSection = document.getElementById('edition-section');
+const btnModify = document.getElementById('btn-modify');
+const filters = document.getElementById('filters');
 
-	authLink.addEventListener("click", () => {
-		localStorage.removeItem("token");
-		window.location.href = "index.html";
-	});
+if (token) {
+	// utilisateur est connecté
+
+	// afficher édition en flex
+	if (editionSection) editionSection.style.display = 'flex';
+	if (btnModify) btnModify.style.display = 'flex';
+
+	// cacher filtres (display none)
+	if (filters) filters.style.display = 'none';
+
+	// lien login en logout (deconnexion + redirection)
+	if (authLink) {
+		authLink.textContent = 'logout';
+		authLink.href = '#'; // href enlevé
+
+		authLink.addEventListener('click', function () {
+			localStorage.removeItem('token');
+			window.location.href = 'index.html';
+		});
+	}
+} else {
+	// utilisateur non connecté
+
+	// n'affiche pas filtres etc...
+	if (editionSection) editionSection.style.display = 'none';
+	if (btnModify) btnModify.style.display = 'none';
+	if (filters) filters.style.display = 'flex';
 }
